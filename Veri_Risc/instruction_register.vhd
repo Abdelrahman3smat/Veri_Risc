b8c616342a9be5934c79fb7e89de41288cd51ab9
Abdelrahman Esmat <145863653+Abdelrahman3smat@users.noreply.github.com>
2024-07-31 17:38:07 +0300

    instruction_register.vhd

--- Veri_Risc/instruction_register.vhd
+++ Veri_Risc/instruction_register.vhd
@@ -1,35 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    09:14:22 07/14/2024 
--- Design Name: 
--- Module Name:    instruction_register - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all; 
 use IEEE.std_logic_arith.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity instruction_register is
     Port ( data : in  STD_LOGIC_VECTOR (7 downto 0);
