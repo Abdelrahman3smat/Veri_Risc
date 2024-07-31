bf56c3b1349106488a8c311be1b7eb3f1d28e318
Abdelrahman Esmat <145863653+Abdelrahman3smat@users.noreply.github.com>
2024-07-31 17:37:41 +0300

    accumulator.vhd

--- Veri_Risc/accumulator.vhd
+++ Veri_Risc/accumulator.vhd
@@ -1,34 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    09:33:30 07/14/2024 
--- Design Name: 
--- Module Name:    accumulator - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity accumulator is
     Port ( alu_out : in  STD_LOGIC_VECTOR (7 downto 0);
