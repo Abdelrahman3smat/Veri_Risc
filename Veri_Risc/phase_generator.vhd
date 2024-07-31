7cf2b466eee3a61cb51bcbf2ca0b918b90df7eae
Abdelrahman Esmat <145863653+Abdelrahman3smat@users.noreply.github.com>
2024-07-31 17:38:42 +0300

    phase_generator.vhd

--- Veri_Risc/phase_generator.vhd
+++ Veri_Risc/phase_generator.vhd
@@ -1,35 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    16:00:01 07/13/2024 
--- Design Name: 
--- Module Name:    phase_generator - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity phase_generator is
     Port ( clk : in  STD_LOGIC;
